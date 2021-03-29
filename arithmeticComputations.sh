8ca51f3d3cd964051f6a94fde9df84f9e6724832
saranamsairakesh <saranamsairakesh@gmail.com>
2021-03-29 13:08:12 +0530

    Added: Arithmetic 2

--- arithmeticComputations.sh
+++ arithmeticComputations.sh
@@ -4,4 +4,5 @@ read -p "Enter Number b= " b
 read -p "Enter Number c= " c
 echo "a= "$a",b= "$b",c= "$c
 arithmetic1=$(($a+$b*$c))
-echo $arithmetic1
+arithmetic2=$(($a*$b+$c))
+echo $arithmetic1,$arithmetic2
