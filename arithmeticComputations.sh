2007d9ffed114696f7484564a1c01c8338427d57
saranamsairakesh <saranamsairakesh@gmail.com>
2021-03-29 13:16:48 +0530

    Added: Storing in Dictionary

--- arithmeticComputations.sh
+++ arithmeticComputations.sh
@@ -2,10 +2,13 @@ echo "Welcome to arithmetic computations"
 read -p "Enter Number a= " a
 read -p "Enter Number b= " b
 read -p "Enter Number c= " c
-echo "a= "$a",b= "$b",c= "$c
+declare -A arithmeticDictionary
 arithmetic1=$(($a+$b*$c))
 arithmetic2=$(($a*$b+$c))
 arithmetic3=$(($c+$a/$b))
 arithmetic4=$(($a%$b+$c))
-echo $arithmetic1,$arithmetic2,$arithmetic3,$arithmetic4
-
+arithmeticDictionary[arith1]=$arithmetic1
+arithmeticDictionary[arith2]=$arithmetic2
+arithmeticDictionary[arith3]=$arithmetic3
+arithmeticDictionary[arith4]=$arithmetic4
+echo ${arithmeticDictionary[@]}
